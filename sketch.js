39bf36ca6d78712ff57ca9fc036a7bb825721203
PuneetSachdev260806 <aditisachdev38@gmail.com>
2020-10-18 17:45:08 +0530

    https added

--- sketch.js
+++ sketch.js
@@ -100,7 +100,7 @@ function keyPressed(){
 }
 
 async function getTime(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
     var responseJson = await response.json();
     var dateTime = responseJson.datetime;
     //console.log(dateTime);
